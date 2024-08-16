bd57f82c52ba9320f408bf9334380fe1d2aa1546
Tino Chirozvi <134059951+tinothecreator@users.noreply.github.com>
2024-08-16 09:00:02 +0200

    tino- i cant run my code yet , but i think what did is corrrect , once im able to run ill test and see if i got it corrrect

--- decrypt_and_print.asm
+++ decrypt_and_print.asm
@@ -1,14 +1,22 @@
 ; ==========================
-; Group member 01: Name_Surname_student-nr
+; Group member 01: Tinotenda_Chirozvi_22547747
 ; Group member 02: Name_Surname_student-nr
 ; Group member 03: Name_Surname_student-nr
 ; ==========================
+
+; Input: 4 values passed to your program (function).
+; Output: Display the message: `The plaintext is: `
+; Function returns: Nothing (Preferebly 0, but this does not matter for this prac).
+; Output snippet:
+; • I call your function like this: `decrypt_and_print(1930506807, 1930507047 , 1930506823, 1930506807)`, you output:
+
+; The plaintext is: TEST
 
 section .data
     fmt db "%c", 0
     ; Do not modify anything above this line unless you know what you are doing
     ; ==========================
-    ; Your data goes here
+    message db "The plaintext is: ",0
     ; ==========================
 
 section .text
@@ -27,8 +35,44 @@ print_char_32:
 decrypt_and_print:
     ; Do not modify anything above this line unless you know what you are doing
     ; ==========================
-    ; Your code goes here
-    ; ==========================
-    ; Do not modify anything below this line unless you know what you are doing
 
+    ; Print the message "The plaintext is: "
+    mov rdi, message
+    xor rax, rax
+    call printf
+
+    ; Load the first argument
+    mov rax, rdi
+    call decrypt_char
+    call print_char_32
+
+    ; Load the second argument
+    mov rax, rsi
+    call decrypt_char
+    call print_char_32
+
+    ; Load the third argument
+    mov rax, rdx
+    call decrypt_char
+    call print_char_32
+
+    ; Load the fourth argument
+    mov rax, rcx
+    call decrypt_char
+    call print_char_32
+
+
+    ret
+
+decrypt_char:
+    ; Rotate Right by 4 Bits.
+    ; XOR with 0x73113777.
+
+    ror rax, 4
+    
+    xor rax, 0x73113777
+    
+    ; The resulting character is in the lowest 8 bits of rax
+    and rax, 0xFF
+    
     ret

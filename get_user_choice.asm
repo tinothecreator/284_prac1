901159d7ac5d3c5b20a75d2df6e81540b487dc73
Tafara7 <hwatatafara4@gmail.com>
2024-08-18 19:53:50 +0200

    fixed greeting and user_choice

--- get_user_choice.asm
+++ get_user_choice.asm
@@ -4,26 +4,37 @@
 ; Group member 03: Devan_Dewet_05169098
 ; ==========================
 
+section .data
+    choice_msg db 'Choice: ', 0  ; Message to be printed before user input
+
 section .bss
-    ; ==========================
-    user_input resb 1 
-    ; ==========================
+    user_input resb 1            ; Reserve 1 byte for user input
 
 section .text
     global get_user_choice
 
-extern greeting             
+extern greeting
 
 get_user_choice:
     ; Call the greeting function to print the welcome message
     call greeting
 
-    ; ==========================
-    mov rax, 0                 
-    mov rdi, 0                  
-    mov rsi, user_input         ; address of buffer to store input
-    mov rdx, 1                  
-    syscall     
-    ; ==========================
+    ; Print "Choice: " message before taking user input
+    mov rax, 1                   
+    mov rdi, 1                  
+    mov rsi, choice_msg          ; address of the "Choice: " message
+    mov rdx, 8                  
+    syscall                     
+
+    ; Get user input
+    mov rax, 0                   
+    mov rdi, 0                   
+    mov rsi, user_input          
+    mov rdx, 1                   ; number of bytes to read
+    syscall                      
+
+    movzx rax, byte [user_input] 
+    sub rax, '0'                 
+
     ; Do not modify anything below this line unless you know what you are doing 
-    ret
+    ret                         
--- greeting.asm
+++ greeting.asm
@@ -1,13 +1,7 @@
 ; ==========================
-<<<<<<< HEAD
 ; Group member 01: Tinotenda_Chirozvi_22547747
 ; Group member 02: Tafara_Hwata_22565991
 ; Group member 03: Devan_Dewet_05169098
-=======
-; Group member 01: Tinotenda Chirozvi_22547747
-; Group member 02: Name_Surname_student-nr
-; Group member 03: Name_Surname_student-nr
->>>>>>> 84c4e39dbef17ae4a14bb2e3b2f03e1de18ac5bf
 ; ==========================
 
 section .data

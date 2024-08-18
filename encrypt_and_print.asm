b98eff1cc17572b280a6c04dd7122dce346d1d3d
DevanDewet <161315233+DevanDewet@users.noreply.github.com>
2024-08-18 17:00:37 +0200

    update

--- encrypt_and_print.asm
+++ encrypt_and_print.asm
@@ -1,13 +1,7 @@
 ; ==========================
-<<<<<<< HEAD
 ; Group member 01: Tinotenda_Chirozvi_22547747
 ; Group member 02: Tafara_Hwata_22565991
 ; Group member 03: Devan_Dewet_05169098
-=======
-; Group member 01: 
-; Group member 02: Tinotenda_Chirozvi_22547747
-; Group member 03: Name_Surname_student-nr
->>>>>>> 84c4e39dbef17ae4a14bb2e3b2f03e1de18ac5bf
 ; ==========================
 ; you can use printf - lecture 7 functions
 
@@ -15,12 +9,13 @@ section .data
     fmt db "%c", 0
     ; Do not modify anything above this line unless you know what you are doing
     ; ==========================
-    ; Your data goes here
+    hex_key dd 0x73113777  ; The XOR key
     ; ==========================
 
 section .bss
     ; ==========================
-    ; Your data goes here
+    input resb 4  ; Reserve 4 bytes for the input
+    cipher resd 4 ; Reserve space for 4 encrypted integers
     ; ==========================
 
 section .text
@@ -45,7 +40,6 @@ encrypt_and_print:
     xor rbx, rbx       ; Clear rbx, which will be used as an index
 
     encrypt_loop:
-
     mov al, byte [rdi + rbx] ; Load one character into al
     rol al, 4                ; Rotate the character left by 4 bits
     movzx eax, al            ; Zero-extend the byte in al to a full register
@@ -56,17 +50,17 @@ encrypt_and_print:
     inc rbx
     loop encrypt_loop
 
-    ; Print the encrypted integers as characters (this assumes a non-standard approach)
+    ; Print the encrypted integers as characters
     lea rsi, [cipher]       ; Load the address of the cipher text
     mov rcx, 4              ; Set the loop counter to 4 (for 4 encrypted integers)
     xor rbx, rbx            ; Reset the index to 0
 
     print_loop:
-
     mov eax, [rsi + rbx*4]  ; Load the encrypted value into eax
     call print_char_32      ; Print the character
     inc rbx                 ; Increment the index
     loop print_loop         ; Repeat for all 4 characters
+
 
     ; Do not modify anything below this line unless you know what you are doing
 
